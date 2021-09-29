11142c8b357d33f11957bec41535de0504182e2d
Christian <you@example.com>
2021-09-29 15:29:33 +0200

    fixing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Peareasy Elastid/i);
+  const linkElement = screen.getByText(/Peareasy Elastics/i);
   expect(linkElement).toBeInTheDocument();
 });
