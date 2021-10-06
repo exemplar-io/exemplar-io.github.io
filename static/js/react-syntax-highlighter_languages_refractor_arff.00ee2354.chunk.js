55f8a718811f04aa6f0b4e11e4c2e3c7bc866f4c
Christian <you@example.com>
2021-10-06 09:54:29 +0200

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_arff.00ee2354.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppe-elastics-frontend"]=this["webpackJsonppe-elastics-frontend"]||[]).push([[8],{139:function(e,t,n){"use strict";function a(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=a,a.displayName="arff",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.00ee2354.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.ec5ccc95.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppe-elastics-frontend"]=this["webpackJsonppe-elastics-frontend"]||[]).push([[60],{186:function(t,n,s){"use strict";function e(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=e,e.displayName="ini",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.ec5ccc95.chunk.js.map
