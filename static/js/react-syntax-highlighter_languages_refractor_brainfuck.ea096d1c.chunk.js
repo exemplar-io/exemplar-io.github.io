26ef27932de9f5bfce304ccb824a3e5a2e05f33e
sasp1 <sebastian.a.specht@gmail.com>
2021-10-12 19:28:31 +0200

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_brainfuck.ea096d1c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppe-elastics-frontend"]=this["webpackJsonppe-elastics-frontend"]||[]).push([[18],{437:function(e,t,a){"use strict";function n(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=n,n.displayName="brainfuck",n.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.ea096d1c.chunk.js.map
