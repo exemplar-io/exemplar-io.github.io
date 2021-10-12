52f6d1dab2f139b57ac4e8e34c3309c62a76f274
Christian Hjelmslund <christianhjelmslund@gmail.com>
2021-10-12 15:33:09 +0200

    added navbar - two empty pages - external link to github (#20)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Peareasy Elastics/i);
+  const linkElement = screen.getByText(/Peareasy Elastics 🔥/i);
   expect(linkElement).toBeInTheDocument();
 });
