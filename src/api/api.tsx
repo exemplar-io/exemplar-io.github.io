071e97837211b27e4e8209dcb4a34e8611666f78
Sebastian <sebastian.a.specht@gmail.com>
2021-10-12 13:26:42 +0200

    updated url to aws backend

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://8eqbhzi5ay.eu-west-1.awsapprunner.com/',
 });
 
 export default instance;
