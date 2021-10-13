6d98829141d3ad260eb57ed394392e4ebace50a0
Christian <you@example.com>
2021-10-13 12:07:46 +0200

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.46009463.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppe-elastics-frontend"]=this["webpackJsonppe-elastics-frontend"]||[]).push([[60],{473:function(t,n,s){"use strict";function e(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=e,e.displayName="ini",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.46009463.chunk.js.map
