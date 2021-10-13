84833c160d28f7e6f58f91eec1bf59a99c4f3bbc
Christian <you@example.com>
2021-10-13 13:19:37 +0200

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_arff.e0d26c20.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppe-elastics-frontend"]=this["webpackJsonppe-elastics-frontend"]||[]).push([[8],{426:function(e,t,n){"use strict";function a(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=a,a.displayName="arff",a.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.e0d26c20.chunk.js.map
--- static/js/react-syntax-highlighter_languages_refractor_brainfuck.731c91a7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppe-elastics-frontend"]=this["webpackJsonppe-elastics-frontend"]||[]).push([[18],{435:function(e,t,a){"use strict";function n(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=n,n.displayName="brainfuck",n.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.731c91a7.chunk.js.map
