d8593a07b2448b6919128a4bc30b7e5ba137ebc0
Sebastian <sebastian.a.specht@gmail.com>
2021-10-13 15:28:29 +0200

    logging auth token

--- src/api/requests/auth.ts
+++ src/api/requests/auth.ts
@@ -2,6 +2,8 @@ import axios from '../api';
 
 export const auth = (code: string) => {
   return axios.post('/github/auth', { githubCode: code }).then((response) => {
+    console.log(response.data);
+
     return response.data;
   });
 };
