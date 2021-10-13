338c331c7107a35b2d606bdbdeae939dfe45851d
Sebastian <sebastian.a.specht@gmail.com>
2021-10-13 09:39:15 +0200

    logging when deleting repos

--- src/api/requests/deleteRepos.ts
+++ src/api/requests/deleteRepos.ts
@@ -4,5 +4,6 @@ export const deleteRepos = async (projectName: string, githubToken: string) => {
   const response = await axios.delete(
     `/github/repo?projectName=${projectName}&token=${githubToken}`,
   );
+  console.log(response);
   return response.data;
 };
